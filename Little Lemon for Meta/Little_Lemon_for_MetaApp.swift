dd61f7561ef35c64ab55e0f3e6314f6ccdec9292
Oleksandr Smakhtin <skiper.smaxtin@icloud.com>
2023-01-05 17:51:02 +0200

    Initial Commit

---
+++ Little Lemon for Meta/Little_Lemon_for_MetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_Lemon_for_MetaApp.swift
+//  Little Lemon for Meta
+//
+//  Created by Oleksandr Smakhtin on 05.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct Little_Lemon_for_MetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
